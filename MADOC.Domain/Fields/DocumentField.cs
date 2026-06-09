7f3ed8b3f1c5c51c8e50178d09a8dc50802360ab
bubus1kk <mishatop173@gmail.com>
2026-06-09 09:49:15 +0300

    made document field model

---
+++ MADOC.Domain/Fields/DocumentField.cs
@@ -0,0 +1,26 @@
+﻿namespace MADOC.Domain.Fields
+{
+    public class DocumentField
+    {
+        public string Key { get; }
+        public string DisplayName { get; }
+        public bool IsRequired { get; }
+
+        public DocumentField(string key, string displayName, bool isRequired)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentException("Ключ поля не может быть пустым", nameof(key));
+            }
+
+            if (string.IsNullOrWhiteSpace(displayName))
+            {
+                throw new ArgumentException("Отображаемое имя не может быть пустым", nameof(displayName));
+            }
+
+            this.Key = key;
+            this.DisplayName = displayName;
+            this.IsRequired = isRequired;
+        }
+    }
+}
