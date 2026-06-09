035b9fc2c594eecc2c365442535614dc3ff74efa
unknown <kruckov3006@yandex.ru>
2026-06-09 11:16:42 +0300

    made filed document model and filedValue model

---
+++ MADOC.Domain/Values/FieldValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MADOC.Domain.Values
+{
+    public class FieldValue
+    {
+        public string FieldKey { get; }
+        public object? Value { get; }
+
+        public FieldValue(string fieldKey, object? value)
+        {
+            if (string.IsNullOrWhiteSpace(fieldKey))
+                throw new ArgumentException("Ключ поле не может быть пустым.", nameof(fieldKey));
+            FieldKey = fieldKey;
+            Value = value;
+        }
+    }
+}
