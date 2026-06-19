bb253893487343fb0f53a9ca547cce7e6ed93ec2
bubus1kk <mishatop173@gmail.com>
2026-06-19 12:37:43 +0300

    refacror:made list option key converter

---
+++ MADOC.Domain/Validation/Lists/ListOptionKeyConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace MADOC.Domain.Validation.Lists
+{
+    public static class ListOptionKeyConverter
+    {
+        public static bool TryConvert(object? value, out ListOptionKey key)
+        {
+            if (value is ListOptionKey listOptionKey)
+            {
+                key = listOptionKey;
+                return true;
+            }
+
+            if (value is string stringValue && !string.IsNullOrWhiteSpace(stringValue))
+            {
+                key = new ListOptionKey(stringValue);
+                return true;
+            }
+
+            key = default;
+            return false;
+        }
+    }
+}
