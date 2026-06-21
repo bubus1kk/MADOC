e6493184b4db43b02ab0c69c93b25a4790e5ae23
bubus1kk <mishatop173@gmail.com>
2026-06-21 17:50:19 +0300

    fix: fix after artemych fail

--- MADOC.Tests/Validation/ListConstraintAttributeTests.cs
+++ MADOC.Tests/Validation/ListConstraintAttributeTests.cs
@@ -1,91 +1,199 @@
-//using MADOC.Domain.Validation.Attributes;
-//using System;
-//namespace MADOC.Tests.Domain.Attributes;
+using System.ComponentModel.DataAnnotations;
+using MADOC.Domain.Validation.Attributes;
+using MADOC.Domain.Validation.ListDependencies;
+using MADOC.Domain.Validation.Lists;
 
-//[TestClass]
-//public class ListConstraintAttributeTests
-//{
-//    [TestMethod]
-//    public void Should_Pass_When_Value_Is_In_Allowed_Values()
-//    {
-//        var model = new ListModel
-//        {
-//            Value = "Студент"
-//        };
+namespace MADOC.Tests.Domain.Attributes;
 
-//        Assert.IsTrue(ValidationTestHelper.IsValid(model));
-//    }
+[TestClass]
+public class ListConstraintAttributeTests
+{
+    private static readonly ListOption Student = new(new ListOptionKey("test.student"),"Студент");
 
-//    [TestMethod]
-//    public void Should_Fail_When_Value_Is_Not_In_Allowed_Values()
-//    {
-//        var model = new ListModel
-//        {
-//            Value = "Преподаватель"
-//        };
+    private static readonly ListOption GroupLeader = new(new ListOptionKey("test.group_leader"),"Староста");
 
-//        Assert.IsFalse(ValidationTestHelper.IsValid(model));
-//    }
+    private static readonly ListOption Graduate = new(new ListOptionKey("test.graduate"),"Выпускник");
 
-//    [TestMethod]
-//    public void Should_Pass_When_Value_Is_Null()
-//    {
-//        var model = new NullableListModel
-//        {
-//            Value = null
-//        };
+    [TestMethod]
+    public void Should_Pass_When_Key_Is_In_Catalog()
+    {
+        var model = new ListModel
+        {
+            Value = Student.Key
+        };
 
-//        Assert.IsTrue(ValidationTestHelper.IsValid(model));
-//    }
+        Assert.IsTrue(IsValid(model));
+    }
 
-//    [TestMethod]
-//    public void Should_Pass_When_Value_Is_Empty_String()
-//    {
-//        var model = new ListModel
-//        {
-//            Value = ""
-//        };
+    [TestMethod]
+    public void Should_Fail_When_Key_Is_Not_In_Catalog()
+    {
+        var model = new ListModel
+        {
+            Value = new ListOptionKey("test.teacher")
+        };
 
-//        Assert.IsTrue(ValidationTestHelper.IsValid(model));
-//    }
+        Assert.IsFalse(IsValid(model));
+    }
 
-//    [TestMethod]
-//    public void Should_Pass_When_Value_Is_Whitespace()
-//    {
-//        var model = new ListModel
-//        {
-//            Value = "   "
-//        };
+    [TestMethod]
+    public void Should_Pass_When_Value_Is_Null()
+    {
+        var model = new ListModel
+        {
+            Value = null
+        };
 
-//        Assert.IsTrue(ValidationTestHelper.IsValid(model));
-//    }
+        Assert.IsTrue(IsValid(model));
+    }
 
-//    [TestMethod]
-//    public void Should_Use_ToString_For_Value_Checking()
-//    {
-//        var model = new ObjectListModel
-//        {
-//            Value = 123
-//        };
+    [TestMethod]
+    public void Should_Pass_When_Value_Is_String_Key_From_Catalog()
+    {
+        var model = new StringListModel
+        {
+            Value = Student.Key.Value
+        };
 
-//        Assert.IsTrue(ValidationTestHelper.IsValid(model));
-//    }
+        Assert.IsTrue(IsValid(model));
+    }
 
-//    private class ListModel
-//    {
-//        [ListConstraint("Студент", "Староста", "Выпускник")]
-//        public string Value { get; set; } = string.Empty;
-//    }
+    [TestMethod]
+    public void Should_Fail_When_Value_Cannot_Be_Converted_To_Key()
+    {
+        var model = new ObjectListModel
+        {
+            Value = 123
+        };
 
-//    private class NullableListModel
-//    {
-//        [ListConstraint("Студент", "Староста", "Выпускник")]
-//        public string? Value { get; set; }
-//    }
+        Assert.IsFalse(IsValid(model));
+    }
 
-//    private class ObjectListModel
-//    {
-//        [ListConstraint("123")]
-//        public object? Value { get; set; }
-//    }
-//}
+    [TestMethod]
+    public void Should_Fail_When_Document_Does_Not_Provide_List_Configuration()
+    {
+        var model = new DocumentWithoutConfiguration
+        {
+            Value = Student.Key
+        };
+
+        Assert.IsFalse(IsValid(model));
+    }
+
+    [TestMethod]
+    public void Should_Fail_When_Field_Is_Not_Described_In_Catalog()
+    {
+        var model = new FieldWithoutCatalogListModel
+        {
+            Value = Student.Key
+        };
+
+        Assert.IsFalse(IsValid(model));
+    }
+
+    private static bool IsValid(object model)
+    {
+        var validationResults = new List<ValidationResult>();
+
+        return Validator.TryValidateObject(
+            model,
+            new ValidationContext(model),
+            validationResults,
+            true);
+    }
+
+    private static DocumentListCatalog CreateCatalog()
+    {
+        var catalog = new DocumentListCatalog();
+
+        catalog.AddList(
+            nameof(ListModel.Value),
+            Student,
+            GroupLeader,
+            Graduate);
+
+        return catalog;
+    }
+
+    private class ListModel : IListConfigurationProvider
+    {
+        private static readonly DocumentListCatalog Catalog = CreateCatalog();
+        private static readonly ListDependencySchema Schema = new();
+
+        [ListConstraint]
+        public ListOptionKey? Value { get; set; }
+
+        public DocumentListCatalog GetListCatalog()
+        {
+            return Catalog;
+        }
+
+        public ListDependencySchema GetListDependencySchema()
+        {
+            return Schema;
+        }
+    }
+
+    private class StringListModel : IListConfigurationProvider
+    {
+        private static readonly DocumentListCatalog Catalog = CreateCatalog();
+        private static readonly ListDependencySchema Schema = new();
+
+        [ListConstraint]
+        public string? Value { get; set; }
+
+        public DocumentListCatalog GetListCatalog()
+        {
+            return Catalog;
+        }
+
+        public ListDependencySchema GetListDependencySchema()
+        {
+            return Schema;
+        }
+    }
+
+    private class ObjectListModel : IListConfigurationProvider
+    {
+        private static readonly DocumentListCatalog Catalog = CreateCatalog();
+        private static readonly ListDependencySchema Schema = new();
+
+        [ListConstraint]
+        public object? Value { get; set; }
+
+        public DocumentListCatalog GetListCatalog()
+        {
+            return Catalog;
+        }
+
+        public ListDependencySchema GetListDependencySchema()
+        {
+            return Schema;
+        }
+    }
+
+    private class FieldWithoutCatalogListModel : IListConfigurationProvider
+    {
+        private static readonly DocumentListCatalog Catalog = new();
+        private static readonly ListDependencySchema Schema = new();
+
+        [ListConstraint]
+        public ListOptionKey? Value { get; set; }
+
+        public DocumentListCatalog GetListCatalog()
+        {
+            return Catalog;
+        }
+
+        public ListDependencySchema GetListDependencySchema()
+        {
+            return Schema;
+        }
+    }
+
+    private class DocumentWithoutConfiguration
+    {
+        [ListConstraint]
+        public ListOptionKey? Value { get; set; }
+    }
+}
