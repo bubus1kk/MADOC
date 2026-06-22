48b84369bf2aea2804c22d4ed59f5a4c7737ac65
bubus1kk <mishatop173@gmail.com>
2026-06-22 22:40:28 +0300

    feat: made anchor definition file (#33)

---
+++ MADOC.Domain/Printing/Anchors/AnchorDefinition.cs
@@ -0,0 +1,40 @@
+﻿namespace MADOC.Domain.Printing.Anchors;
+
+public sealed class PrintAnchorDefinition<TDocument> where TDocument : class
+{
+    private readonly Func<TDocument, object?> getValueFromDocument;
+
+    public AnchorKey Key { get; }
+
+    public string FieldName { get; }
+
+    public PrintAnchorDefinition(AnchorKey key, string fieldName, Func<TDocument, object?> getValueFromDocument)
+    {
+        ValidateFieldName(fieldName);
+        ArgumentNullException.ThrowIfNull(getValueFromDocument);
+
+        Key = key;
+        FieldName = fieldName;
+        this.getValueFromDocument = getValueFromDocument;
+    }
+
+    public object? GetValue(TDocument document)
+    {
+        ValidateDocument(document);
+
+        return getValueFromDocument(document);
+    }
+
+    private static void ValidateFieldName(string fieldName)
+    {
+        if (string.IsNullOrWhiteSpace(fieldName))
+        {
+            throw new ArgumentException("Имя поля якоря не может быть пустым", nameof(fieldName));
+        }
+    }
+
+    private static void ValidateDocument(TDocument document)
+    {
+        ArgumentNullException.ThrowIfNull(document);
+    }
+}
